870218ea01d23c0696a8843b31b6431ccd6bc464
ahmedhassank <92717109+ahmedhassank@users.noreply.github.com>
2022-06-05 12:59:08 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,12 @@
+sudo apt-get install python3-pyqt5 -y
+sudo apt-get install pyqt5-dev-tools -y
+sudo apt-get install qttools5-dev-tools -y
+sudo apt update
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+sudo apt update
+apt-cache policy docker-ce
+sudo apt install docker-ce -y
+sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
