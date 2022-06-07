0853c36e2832dceab0ef38ec2f517958b67ca06d
ahmedhassank <92717109+ahmedhassank@users.noreply.github.com>
2022-06-07 21:28:58 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,13 @@
-sudo apt-get install python3-pyqt5 -y
-sudo apt-get install pyqt5-dev-tools -y
-sudo apt-get install qttools5-dev-tools -y
-sudo apt update
-sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+apt-get install python3-pyqt5 -y
+apt-get install pyqt5-dev-tools -y
+apt-get install qttools5-dev-tools -y
+apt update
+apt install apt-transport-https ca-certificates curl software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
-sudo apt update
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+apt update
 apt-cache policy docker-ce
-sudo apt install docker-ce -y
-sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+apt install docker-ce -y
+curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+sysctl -w vm.max_map_count=262144
